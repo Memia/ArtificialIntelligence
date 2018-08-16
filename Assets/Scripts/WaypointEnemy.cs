7a87616801eec9f7188753e7bfe656345c634bc7
Memia <memia@hotmail.com.au>
2018-08-16 10:22:00 +1000

    Week02_Homework
    I made AI navigate to the waypoints using navigation map.

--- Assets/Scripts/WaypointEnemy.cs
+++ Assets/Scripts/WaypointEnemy.cs
@@ -1,37 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class WaypointEnemy : MonoBehaviour
+
 {
     public Transform waypointParent;
-    //Creates a collection of transforms
+    public NavMeshAgent agent;
+
+    //Creates a collection(array) of transforms
     private Transform[] waypoints;
     private int currentIndex = 1;
 
 
-    // Use this for initialization
     void Start()
     {
         //Getting children of waypointParent
         waypoints = waypointParent.GetComponentsInChildren<Transform>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+   
         Transform point = waypoints[currentIndex];
         float distance = Vector3.Distance(transform.position, point.position);
-        if (distance < .5f)
+        
+        if (distance < 10.10f)
         {
+          
             // currentIndex = currentIndex +1
             currentIndex++;
-        }
+        }//makes the index 1 if index reach/exceeds range, use this to create cycle
        if (currentIndex >= waypoints.Length)
         {
             currentIndex = 1;
         }
-        
-        transform.position = Vector3.MoveTowards(transform.position, point.position, 0.1f);
+        agent.SetDestination(point.position);
+        //transform.position = Vector3.MoveTowards(transform.position, point.position, 0.1f);
     }
 }
