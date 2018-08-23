39eb9f952e7c8b3cbb2c84868442b2c94a62dd2f
Memia <memia@hotmail.com.au>
2018-08-23 12:09:32 +1000

    Week05
    We made it so the agent follows where the cursor is

---
+++ Assets/SteeringBehaviours/Scripts/AI/AIAgent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+namespace SteeringBehaviours
+{
+    public class AIAgent : MonoBehaviour
+    {
+        #region Variables
+        public NavMeshAgent agent;
+        private Vector3 point;
+        #endregion
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (point.magnitude > 0)
+            {
+                agent.SetDestination(point);
+            }
+        
+        }
+        public void SetTarget(Vector3 point)
+        {   //this defines what you are refering to
+            this.point = point;
+        }
+    }
+
+}
---
+++ Assets/SteeringBehaviours/Scripts/AI/AIAgentDirector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SteeringBehaviours
+{
+    public class AIAgentDirector : MonoBehaviour
+    {
+        public AIAgent agent;
+
+        // Use this for initialization
+        void FixedUpdate()
+        {
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(camRay, out hit, 1000f))
+            {
+                agent.SetTarget(hit.point);
+            }
+        }
+       
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
